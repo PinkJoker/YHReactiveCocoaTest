9a5063578ad31c2b4b86f810853b73266d585312
我叫MT <forever89678@163.com>
2016-11-30 15:26:20 +0800

    no message

--- YHReactiveCocoa/YHReactiveCocoa/SecondViewController.m
+++ YHReactiveCocoa/YHReactiveCocoa/SecondViewController.m
@@ -88,7 +88,19 @@
     //用于当一个信号，被多次订阅时，为了保证创建信号时，避免多次调用创建信号中的block，造成副作用，使用这个类处理
     //注意事项:RACMulticastConnection通过RACSignal的-publish或者-muticast:创建
     //RACMulticastConnection使用步骤
-    //1.创建信号 +(RACSignal *)createSignal:(RACDisposable)
+    //1.创建信号 +(RACSignal *)createSignal:(RACDisposable 。。。。。)。。。。。
+    //2.创建连接 RACMulticastConnection *connect = [signal publish]
+    //3.订阅信号，注意:订阅的不在是之前的信号，而是连接的信号 [connect.signal subscribeNext.....]
+    //4.连接 [connect connect]
+    
+    //RACMulticastConnection底层原理
+    //1.创建connect,connect.sourceSignal ->RACSignal(原始信号)  connect.signal - >RACSubject
+    //2.订阅connect.signal,会调用RACSubject的subscribeNext,创建订阅者，而且把订阅者保存起来，不会执行block
+    //3.[connect connect]内部会订阅RACSignal(原始信号)，并且订阅者是RACSubject
+    //3.1 订阅原始信号，就会调用原始信号中的didSubscribe
+    //3.2didSubscribe拿到订阅者调用sendNext,其实是调用RACSubject的sendNext
+    //4.RACSubject的sendNext,会遍历RACSubject所有订阅者发送信号
+    //4.1因为刚刚第二部，都是在订阅RACSubject，因此会拿到第二部所有的订阅者
     
     
     
